4cb2e946096472fedbb47de5d0705e29f3f4ec2c
kurukuruzz <112538549+kurukuruzz@users.noreply.github.com>
2023-06-27 19:20:13 +0900

    view를 ejs로 변환

--- main.js
+++ main.js
@@ -32,33 +32,30 @@ var addUser = function(name, password, email, callback){
 }
 
 var authUser = function(email, password, callback) {
-    console.log('22222222222222');
     pool.getConnection(function(err, conn) {
         if(err) {
             conn.release();
             return;
         }
-        console.log(email);
-        console.log(password);
         var columns = ['email', 'password'];
         var tablename = 'user';
 
         var exec = conn.query("select ?? from ?? where email = ? and password = ?",
             [columns, tablename, email, password], function(err, rows) {
-                console.log(rows);
                 conn.release();
-                console.log('실행 대상SQL: ' + exec.sql);
+                console.log('대상SQL: ' + exec.sql);
             if(rows.length > 0) {
-                console.log('일치');
                 callback(null, rows);
                 return;
             } else {
-                console.log('못찾음');
                 callback(null, null);
             }
         });
     });
 }
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + "/public"));
 
@@ -77,37 +74,31 @@ app.get('/singup', (req, res) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/singin', (req, res) => {
-    console.log('3333333333333');
-    var paramEmail = req.param('email');
-    var paramPassword = req.param('password');
-
-    console.log(paramEmail);
+    var paramEmail = req.body.email;
+    var paramPassword = req.body.password;
 
     if(pool){
         authUser(paramEmail, paramPassword, function(err, rows) {
-            if(err){
-                throw err;
-            }
+            if(err) { throw err; }
+            
             if(rows){
-                res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
-                res.write('<h1>로그인 성공</h1>');
-                res.write('<div><p>사용자 이메일:' + paramEmail + '</p></div>');
-                res.write('<div><p>사용자 비밀번호:' + paramPassword + '</p></div>');
-                res.write("<br><br><a href='/'>메인페이지로 이동하기</a></br></br>");
-                res.end();
+                var context = {paramEmail : paramEmail, paramPassword : paramPassword};
+                res.render('login_success', context, function(err, html) {
+                    if(err) { throw err; }
+                    res.end(html);
+                });
             } else {
-                res.writeHead('200', { 'Content-Type': 'text/html;charset=utf8' });
-                res.write('<h1>로그인 실패</h1>');
-                res.write('<hr/>');
-                res.write('<div><p>다른 아이디 또는 비밀번호를 입력하세요</p></div>');
-                res.write("<br><br><a href='/singin'>다시 로그인 하기</a></br></br>");
-                res.end();
+                res.render('login_failed', '', function(err, html) {
+                    if(err) { throw err; }
+                    res.end(html);
+                });
             }
         });
     } else {
-        res.writeHead('200', {'Content-Type': 'text/html; charset=utf8'});
-        res.write('<h2>데이터 베이스 연결 실패</h2>');
-        res.end();
+        res.render('database_failed', '', function(err, html){
+            if(err) { throw err; }
+            res.end(html);
+        });
     }
 });
 
@@ -118,9 +109,7 @@ app.post('/singup', (req, res) => {
 
     if(pool) {
         addUser(paramName, paramPassword, paramEmail), function(err, result) {
-            if(err) {
-                throw err;
-            }
+            if(err) { throw err; }
 
             if(result) {
                 res.writeHead('200', {'Content-Type': 'text/html; charset=utf8'});
@@ -133,15 +122,16 @@ app.post('/singup', (req, res) => {
             }
         }
     } else {
-        res.writeHead('200', {'Content-Type': 'text/html; charset=utf8'});
-        res.write('<h2>데이터 베이스 연결 실패</h2>');
-        res.end();
+        res.render('database_failed', '', function(err, html){
+            if(err) { throw err; }
+            res.end(html);
+        });
     } 
-    res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
-    res.write('<h1>회원가입 성공</h1>');
-    res.write("<br><br><a href='/'>메인페이지로 돌아가기</a></br></br>");
-    res.write("<br><br><a href='/singin'>로그인 페이지로 이동하기</a></br></br>");
-    res.end();
+
+    res.render('singup_success', '', function(err, html) {
+        if(err) { throw err; }
+        res.end(html);
+    });
 });
 
 
